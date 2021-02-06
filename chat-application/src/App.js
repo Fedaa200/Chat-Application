b40e0f3cc980daa672b59067a9e08e8d8cead6ed
Fedaa <fadooshjad2001@gmail.com>
2021-02-06 18:14:52 +0200

    done

--- chat-application/src/App.js
+++ chat-application/src/App.js
@@ -1,15 +1,15 @@
 import { ChatEngine } from "react-chat-engine";
 import "./App.css";
-// import LoginForm from "./components/LoginForm";
+import LoginForm from "./components/LoginForm";
 import ChatFeed from "./components/ChatFeed";
 const App = () => {
-  // if (!localStorage.getItem("username")) return <LoginForm />;
+  if (!localStorage.getItem("username")) return <LoginForm />;
   return (
     <ChatEngine
       height="620px"
       projectID="cbcd8d86-17a1-4ce9-93c3-b5f3dfb272ad"
-      userName="fedaa2001"
-      userSecret="123123"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
       renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
     />
   );
